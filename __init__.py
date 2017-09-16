33e41bc0cada53c3c6c0a16d1d7f3874187e0674
Abhishek <abhishekasdzxc@gmail.com>
2017-09-16 15:30:29 +0530

    removed the extra lines

--- __init__.py
+++ __init__.py
@@ -2,18 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pickle as p
 from .activations import *
-
-def loss(type,Y,A_out):
-    if type=='logistic':
-        return -(np.multiply(Y,np.log(A_out))+np.multiply((1-Y),np.log(1-A_out)))
-    if type=='mean_square_error':
-        return np.square(A_out-Y)
-
-def loss_prime(type,Y,A_out):
-    if type=='logistic':
-        return -(np.divide(Y,A_out)-np.divide((1-Y),(1-A_out)))
-    if type=='mean_square_error':
-        return A_out-Y
+from .losses import *
 
 def load_data(filename,train=False, test=False):
     """
